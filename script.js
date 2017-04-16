39ead48498ec727ef4f2fd2ee0e52dce2be1abb3
root <root@ip-172-31-37-185.eu-west-1.compute.internal>
2017-04-16 18:19:20 +0000

    nyt ois perustoiminta

--- script.js
+++ script.js
@@ -1,46 +1,44 @@
 var xhttp = new XMLHttpRequest();
-
-$.getJSON( "test.json", function( data ) {
-    var items = [];
-    data.sort(function(a,b){return a.votes - b.votes});
-    data.reverse();
-    $.each( data, function( i, item) {
-        items.push( "<li id='" +data[i].votes + "'>" + data[i].idea + ": " + data[i].votes + "</li>" );
+function loadVotes(){
+    $.getJSON( "http://veli.vodka:8080/api", function( data ) {
+        $(".topBox").empty();
+        var items = [];
+        //data.sort(function(a,b){return a.votes - b.votes});
+        //data.reverse();
+        $.each( data, function( i, item) {
+            items.push( "<button id='" +data[i].idea + "' class='foo' data-id=" +data[i].idea + " style='width: calc(100px + " + data[i].votes * 10 + "px)' >" + data[i].idea + ": " + data[i].votes + "</button> </br>" );
+        });
+        $( "<ul/>", {
+            "class": "votes-list",
+            html: items.join( "" )
+        }).appendTo( ".topBox" );
+        $('.foo').click (voteClicked);
     });
-    $( "<ul/>", {
-        "class": "votes-list",
-        html: items.join( "" )
-    }).appendTo( ".topBox" );
-});
+};
 
 
-function newVoteClicked(){
-    console.log("KAKKANAAMA!");
-    xhttp.open("POST", "http://veli.vodka:80/", true);
+function voteClicked(event){
+    console.log(event.target.dataset.id);
+    console.log($("#idea").val());
+    xhttp.open("POST", "http://tbd.veli.vodka:8080/api/vote", true);
     xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xhttp.send("idea=kakkanaama");
-}
-
-function post(path, params, method) {
-    method = method || "post"; // Set method to post by default if not specified.
-
-    // The rest of this code assumes you are not using a library.
-    // It can be made less wordy if you use one.
-    var form = document.createElement("form");
-    form.setAttribute("method", method);
-    form.setAttribute("action", path);
-
-    for(var key in params) {
-        if(params.hasOwnProperty(key)) {
-            var hiddenField = document.createElement("input");
-            hiddenField.setAttribute("type", "hidden");
-            hiddenField.setAttribute("name", key);
-            hiddenField.setAttribute("value", params[key]);
-
-            form.appendChild(hiddenField);
-         }
+    xhttp.onreadystatechange = function(){
+        if (this.readyState == 4 && this.status == 200) {
+            loadVotes();
+        }
     }
+    xhttp.send("idea=" + event.target.dataset.id)
+};
+function newVoteClicked(){
+    console.log($("#idea").val());
+    xhttp.open("POST", "http://tbd.veli.vodka:8080/api", true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.onreadystatechange = function(){
+        if (this.readyState == 4 && this.status == 200) {
+            loadVotes();
+        }
+    }
+    xhttp.send("idea=" + $("#idea").val())
+};
 
-    document.body.appendChild(form);
-    form.submit();
-}
+window.onload = loadVotes;
